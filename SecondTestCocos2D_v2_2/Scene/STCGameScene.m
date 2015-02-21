366a8d88c69ab67a272316f19fd98b4083819c4c
Admin <kneo82@gmail.com>
2015-02-21 14:19:17 +0300

    Add score label and health label

--- SecondTestCocos2D_v2_2/Scene/STCGameScene.m
+++ SecondTestCocos2D_v2_2/Scene/STCGameScene.m
@@ -8,12 +8,30 @@
 
 #import "STCGameScene.h"
 
+typedef NS_ENUM(NSUInteger, STCNodesName) {
+    kSTCNodeNameScoreLabel              = 100,
+    kSTCNodeNamePlayerHealthBackground  = 120,
+    kSTCNodeNamePlayerHealth            = 125,
+};
+
+static const NSUInteger kSTCBarHeightSize       = 45;
+static const NSUInteger kSTCBarFontSize         = 20;
+static const NSUInteger kSTCHealthBarFontSize   = 14;
+static NSString * const kSTCScoreFontName       = @"Thirteen Pixel Fonts";
+static NSString * const kSTCHealthBarFontName   = @"Arial";
+static NSString * const kSTCScoreLabel          = @"Score: 0";
+static NSString * const kSTCHealthBar           = @"=======================================";
+;
+
 @interface STCGameScene ()
 @property (nonatomic, assign)   CGSize  winSize;
-@property (nonatomic, strong)   CCLayer *playerLaerNode;
-@property (nonatomic, strong)   CCLayer *hudLaerNode;
+@property (nonatomic, strong)   CCLayer         *playerLaerNode;
+@property (nonatomic, strong)   CCLayer         *hudLaerNode;
+@property (nonatomic, strong)   CCAction        *scoreFlashAction;
+@property (nonatomic, strong)   CCLabelBMFont   *playerHealthLabel;
 
 - (void)setupSceneLayer;
+- (void)setupUI;
 
 @end
 
@@ -48,6 +66,7 @@
         self.winSize = [CCDirector sharedDirector].winSize;
 
         [self setupSceneLayer];
+        [self setupUI];
         
         
 //        CCLabelTTF *label = [CCLabelTTF labelWithString:@"Thirteen Pixel Fonts" fontName:@"Thirteen Pixel Fonts" fontSize:40];
@@ -98,5 +117,80 @@
     [self addChild:self.hudLaerNode];
 }
 
+- (void)setupUI {
+    CGSize size = self.winSize;
+    
+    CGSize backgroundSize = CGSizeMake(size.width, kSTCBarHeightSize);
+    ccColor4B backgroundColor = ccc4BFromccc4F(ccc4f(0.5, 0, 0.05, 1));
+    
+    CCLayerColor *hudBarBackground = [CCLayerColor layerWithColor:backgroundColor
+                                                            width:backgroundSize.width
+                                                           height:backgroundSize.height];
+    
+    hudBarBackground.position = ccp(0, size.height - kSTCBarHeightSize);
+    
+    hudBarBackground.anchorPoint = CGPointZero;
+    [self.hudLaerNode addChild:hudBarBackground];
+    
+    CCLabelTTF *scoreLabel = [CCLabelTTF labelWithString:kSTCScoreLabel
+                                                fontName:kSTCScoreFontName
+                                                fontSize:kSTCBarFontSize];
+    
+    scoreLabel.position = ccp(size.width / 2, size.height - scoreLabel.contentSize.height / 2);
+    scoreLabel.tag = kSTCNodeNameScoreLabel;
+    
+    [self.hudLaerNode addChild:scoreLabel];
+    
+    CCAction *scaleInAction = [CCScaleTo actionWithDuration:0.1 scale:1.5];
+    CCAction *scaleOutAction = [CCScaleTo actionWithDuration:0.1 scale:1.0];
+    CCSequence *flashAction = [CCSequence actionWithArray:@[scaleInAction, scaleOutAction]];
+    CCAction *repeartAction = [CCRepeat actionWithAction:flashAction times:20];
+    
+    CCSequence *fulFlashAction = [CCSequence actionWithArray:@[repeartAction, scaleOutAction]];
+    
+    [scoreLabel runAction:fulFlashAction];
+    
+    self.playerHealthLabel = [CCLabelTTF labelWithString:kSTCHealthBar fontName:kSTCHealthBarFontName fontSize:0];
+    
+    CCLabelTTF *playerHealthBackgroundLabel = [CCLabelTTF labelWithString:kSTCHealthBar
+                                                                 fontName:kSTCHealthBarFontName
+                                                                 fontSize:kSTCHealthBarFontSize];
+    
+    playerHealthBackgroundLabel.tag = kSTCNodeNamePlayerHealthBackground;
+    playerHealthBackgroundLabel.color = ccGRAY;
+    
+    CGFloat yPosition = size.height - hudBarBackground.contentSize.height - kSTCHealthBarFontSize / 3;
+    CGPoint position = ccp(0, yPosition);
+    playerHealthBackgroundLabel.position = position;
+    playerHealthBackgroundLabel.anchorPoint = ccp(0, 0);
+
+    [self.hudLaerNode addChild:playerHealthBackgroundLabel];
+    
+    
+    CGFloat testHealth = 75;
+    NSString * actualHealth = [kSTCHealthBar substringToIndex:
+                               (testHealth / 100 * kSTCHealthBar.length)];
+    
+    CCLabelTTF *playerHealthLabel = [CCLabelTTF labelWithString:actualHealth
+                                                       fontName:kSTCHealthBarFontName
+                                                       fontSize:kSTCHealthBarFontSize];
+    
+    playerHealthLabel.tag = kSTCNodeNamePlayerHealth;
+    playerHealthLabel.color = ccGREEN;
+    
+//    CGFloat yPosition = size.height - hudBarBackground.contentSize.height - kSTCHealthBarFontSize / 3;
+//    CGPoint position = ccp(0, yPosition);
+    playerHealthLabel.position = position;
+    playerHealthLabel.anchorPoint = ccp(0, 0);
+    
+    [self.hudLaerNode addChild:playerHealthLabel];
+    
+
+//    playerHealthLabel.name = "playerHealthLabel" playerHealthLabel.fontColor = SKColor.greenColor() playerHealthLabel.fontSize = 50 playerHealthLabel.text =
+//    healthBarString.substringToIndex(20*75/100) playerHealthLabel.horizontalAlignmentMode = .Left playerHealthLabel.verticalAlignmentMode = .Top playerHealthLabel.position = CGPoint(
+//                                                                                                                                                                                      x: CGRectGetMinX(playableRect),
+//                                                                                                                                                                                      y: size.height - CGFloat(hudHeight) + playerHealthLabel.frame.size.height) hudLayerNode.addChild(playerHealthLabel)
+//    ￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼
+}
 
 @end
